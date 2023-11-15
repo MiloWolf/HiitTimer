63d1e606b9b2e8e2c142a4cfb7c9b36935d6f743
MiloWolf <43528473+MiloWolf@users.noreply.github.com>
2023-11-15 12:24:11 -0600

    Pause Button add & Reset updated
    Pause/Resume button functionality has been added.  And Reset button functionality has been updated to clear DOM.
    Finished! Legend has been repositioned and updated.

--- script.js
+++ script.js
@@ -14,17 +14,18 @@ setInterval(() => {
 }, 1000);
 
 const startButton = document.getElementById("startButton");
-const stopButton = document.getElementById("stopButton");
+const pauseButton = document.getElementById("pauseButton");
 const resetButton = document.getElementById("resetButton");
 const ronda = document.getElementById("ronda");
 const state = document.getElementById("state");
 
-dynVars = [];
+dynVars = [1, 1, 1, 0];
+let y = 0;
+// dynVars = [];
 
-///////////////////////////////////Take al inputs//////////////////////////////////
+///////////////////////////////////Take all inputs//////////////////////////////////
 function extractTime(input, varName) {
   const parts = input.split(":");
-  let errorID = "error" + varName;
 
   if (parts.length === 2) {
     const minutes = parseInt(parts[0]);
@@ -32,34 +33,15 @@ function extractTime(input, varName) {
     dynVars.push(minutes * 60 + seconds);
 
     if (!isNaN(minutes) && !isNaN(seconds)) {
-      document.getElementById(errorID).textContent = "🏋🏻‍♂️";
+      error.innerHTML = "🟢";
     } else {
-      document.getElementById(errorID).textContent =
-        'Invalid input. Please use the format "minutes:seconds".';
+      error.innerHTML = `🔴Invalid input. Please use the format "minutes:seconds".+${varName}`;
     }
   } else {
-    document.getElementById("result").textContent =
-      'Invalid input. Please use the format "minutes:seconds".';
+    error.innerHTML = `🔴Invalid input. Please use the format "minutes:seconds".+${varName}`;
   }
 }
-////////////////////////////Hiit Timer function /////////////////////////////////////
-// Function to async countdowns
-function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-let y = 0;
-// Countdown function for interval and rest
-async function countdown(number, type) {
-  for (let i = number; i >= 0; i--) {
-    state.innerHTML = `${type}:`;
-    displayToTimeFormat(i, timerSec, timerMin, timerHrs);
-
-    await delay(1000); // wait for 1 second
-    y++;
-    displayToTimeFormat(y, Ssec, Smin, Shrs);
-  }
-}
-
+//////////////Convert and Display time Format///////////
 function displayToTimeFormat(y, tagSec, tagMin, tagHrs) {
   tagHrs.innerHTML =
     (`${Math.floor(y / 3600)}` < 10 ? "0" : "") + `${Math.floor(y / 3600)}`;
@@ -70,55 +52,112 @@ function displayToTimeFormat(y, tagSec, tagMin, tagHrs) {
   tagSec.innerHTML = (`${remSec % 60}` < 10 ? "0" : "") + `${remSec % 60}`;
 }
 
-// Main function for Timer countdown
-async function hiitTimer([rounds, interval, rest, cool]) {
+///////////////// test area ////////////////////////
+/////////////////test area////////////////////////
+
+// Function to simulate async operation
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// Countdown function
+async function countdown(number, type) {
+  for (let i = number; i >= 0; i--) {
+    //Update DOM Interval/ Rest and Main Timer.
+    state.innerHTML = `${type}`;
+    displayToTimeFormat(i, timerSec, timerMin, timerHrs);
+    await delay(1000); // wait for 1 second
+    //Update DOM Session Timer
+    y++;
+    displayToTimeFormat(y, Ssec, Smin, Shrs);
+  }
+}
+
+// Main function for interval training
+async function hiitTimer(interval, rest, rounds, cool) {
+  let paused = false;
+  //Update DOM the start state of the Rounds
   const r = rounds;
   ronda.innerHTML = "Round: " + `${r}` + "/" + `${r}`;
+
+  // Function to handle pause and resume
+  async function handlePause() {
+    return new Promise((resolve) => {
+      const intervalId = setInterval(() => {
+        if (!paused) {
+          clearInterval(intervalId);
+          resolve();
+        }
+      }, 100);
+    });
+  }
+
+  // Button press event example and update Pause button text
+  document.getElementById("pauseButton").addEventListener("click", () => {
+    paused = !paused;
+    const buttonText = paused ? "Resume" : "Pause";
+    pauseButton.innerText = buttonText;
+  });
+
+  // Loop through rounds
   while (rounds > 0) {
     // Interval countdown
+    await handlePause();
     await countdown(interval, "Interval");
 
     // Rest countdown
     if (rounds > 1) {
+      await handlePause();
       await countdown(rest, "Rest");
     }
 
     rounds--;
+    //Update DOM the following state of the Rounds
     ronda.innerHTML = "Round: " + `${rounds}` + "/" + `${r}`;
   }
 
   // Cool down countdown
+  await handlePause();
   await countdown(cool, "Cool Down");
-  errorInterval.innerHTML = "Complete!";
+  complete.innerHTML = "FINISHED!";
 }
-
-// Example usage with interval = 5, rest = 3, rounds = 3, cool = 10
 
 startButton.addEventListener("click", () => {
   // This will build the output -> dynVars =[rounds, interval, rest, cool down ]
+  // const r = document.getElementById("rounds").value;
+  // dynVars.push(r * 1);
 
-  const r = document.getElementById("rounds").value;
-  dynVars.push(r * 1);
+  // const i = document.getElementById("interval").value;
+  // extractTime(i, "Interval");
 
-  const i = document.getElementById("interval").value;
-  extractTime(i, "Interval");
+  // const re = document.getElementById("rest").value;
+  // extractTime(re, "Rest");
 
-  const re = document.getElementById("rest").value;
-  extractTime(re, "Rest");
+  // const cool = document.getElementById("cooldown").value;
+  // extractTime(cool, "Cooldown");
 
-  const cool = document.getElementById("cooldown").value;
-  extractTime(cool, "Cooldown");
-
+  // console.log(dynVars);
+  // hiitTimer(dynVars);
   // Start the HIIT timer
-
-  console.log(dynVars);
-  hiitTimer(dynVars);
+  hiitTimer(2, 3, 3, 5);
 });
+
+pauseButton.addEventListener("click", () => {});
 
 resetButton.addEventListener("click", () => {
   document.getElementById("rounds").value = "";
   document.getElementById("interval").value = "";
   document.getElementById("rest").value = "";
   document.getElementById("cooldown").value = "";
+  rounds = 0;
+  Ssec.innerHTML = "00";
+  Smin.innerHTML = "00";
+  Shrs.innerHTML = "00";
+  timerSec.innerHTML = "00";
+  timerMin.innerHTML = "00";
+  timerHrs.innerHTML = "00";
+  state.innerHTML = "Interval/Rest";
+  ronda.innerHTML = "Round 0/0";
+  pauseButton.innerHTML = "Pause";
   dynVars = [];
 });
